f49b431bceb7732eb6d60a75f41a98c23b087b72
jakubwilk <jw@jakubwilk.pl>
2023-02-26 20:27:38 +0100

    Install mantine UI framework

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
