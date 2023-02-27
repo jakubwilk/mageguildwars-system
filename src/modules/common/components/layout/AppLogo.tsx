f4742a5f384f51954c031bb82793036bc20b7f4a
jakubwilk <jw@jakubwilk.pl>
2023-02-27 21:55:02 +0100

    Update logo

--- src/modules/common/components/layout/AppLogo.tsx
+++ src/modules/common/components/layout/AppLogo.tsx
@@ -1,8 +1,30 @@
 import Image from 'next/image'
+import { createStyles } from '@mantine/core'
 import logo from '@static/logo.png'
 
+const useStyles = createStyles((theme) => ({
+  logo: {
+    height: 'auto',
+    width: 140,
+    [`@media screen and (min-width: ${theme.breakpoints.sm}px)`]: {
+      height: 49,
+      width: 224,
+    },
+    [`@media screen and (min-width: ${theme.breakpoints.md}px)`]: {
+      height: 56,
+      width: 256,
+    },
+    [`@media screen and (min-width: ${theme.breakpoints.lg}px`]: {
+      height: 70,
+      width: 320,
+    },
+  },
+}))
+
 function AppLogo() {
-  return <Image src={logo} alt={'Mage Guild Wars - PBF Fairy Tail'} height={70} width={320} />
+  const { classes } = useStyles()
+
+  return <Image src={logo} alt={'Mage Guild Wars - PBF Fairy Tail'} className={classes.logo} />
 }
 
 export default AppLogo
