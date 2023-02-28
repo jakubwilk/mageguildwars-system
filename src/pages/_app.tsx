1a8f16b0737425d5859727576656e6f248abaa92
jakubwilk <jw@jakubwilk.pl>
2023-02-28 20:41:19 +0100

    Create custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app'
 import { AppLayout } from '@common'
 import { MantineProvider } from '@mantine/core'
+import { APP_THEME } from '@theme'
 
 import '@styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider theme={APP_THEME} withGlobalStyles withNormalizeCSS>
       <AppLayout>
         <Component {...pageProps} />
       </AppLayout>
