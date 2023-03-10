93175c2ecda8ccd37c8321d62edf14fcbbd55996
jakubwilk <jw@jakubwilk.pl>
2023-03-10 10:46:31 +0100

    Create mage profile page with basic data and modify url

--- src/modules/profile-mage/pages/ProfileMagePage.tsx
+++ src/modules/profile-mage/pages/ProfileMagePage.tsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react'
+import { IMage } from '@profile-mage'
+import { useUserContext } from '@user'
+
 function ProfileMagePage() {
-  return <p>{'ProfileMagePage'}</p>
+  const { user } = useUserContext()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [mages, setMages] = useState<IMage[]>([])
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch('/api/mage')
+      .then((res) => res.json())
+      .then((res) => {
+        setMages(res)
+        setIsLoading(false)
+      })
+  }, [setMages])
+
+  if (!user) {
+    return <p>{'Podany użytkownik nie istnieje'}</p>
+  }
+
+  return (
+    <main>
+      <h2>
+        {'Profil użytkownika: '}
+        {user.username}
+      </h2>
+      {isLoading ? (
+        <p>{'Ładowanie'}</p>
+      ) : (
+        <section>
+          {mages.map((mage) => (
+            <div key={mage.id}>{mage.username}</div>
+          ))}
+        </section>
+      )}
+    </main>
+  )
 }
 
 export default ProfileMagePage
