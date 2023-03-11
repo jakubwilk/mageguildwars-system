7b87dede805722ab331eaf21c34642d54117fc77
jakubwilk <jw@jakubwilk.pl>
2023-03-11 23:55:55 +0100

    Edit mage profile main page

--- src/modules/profile-mage/pages/ProfileMagePage.tsx
+++ src/modules/profile-mage/pages/ProfileMagePage.tsx
@@ -1,9 +1,25 @@
 import { useEffect, useState } from 'react'
+import { important } from '@common'
+import { Button, createStyles } from '@mantine/core'
 import { IMage } from '@profile-mage'
+import { IconUserPlus } from '@tabler/icons'
 import { useUserContext } from '@user'
+
+const useStyles = createStyles(() => ({
+  grid: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  createButton: {
+    display: 'flex',
+    minHeight: '100%',
+    height: important('100%'),
+  },
+}))
 
 function ProfileMagePage() {
   const { user } = useUserContext()
+  const { classes } = useStyles()
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [mages, setMages] = useState<IMage[]>([])
 
@@ -31,9 +47,16 @@ function ProfileMagePage() {
         <p>{'Ładowanie'}</p>
       ) : (
         <section>
-          {mages.map((mage) => (
-            <div key={mage.id}>{mage.username}</div>
-          ))}
+          <div className={classes.grid}>
+            {mages.map((mage) => (
+              <div key={mage.id}>
+                <img src={mage.avatarUrl} alt={mage.username} />
+              </div>
+            ))}
+            <Button className={classes.createButton} radius={0} leftIcon={<IconUserPlus />}>
+              {'Dodaj nową postać'}
+            </Button>
+          </div>
         </section>
       )}
     </main>
