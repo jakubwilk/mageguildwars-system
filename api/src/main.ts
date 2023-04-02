883c82396eb03888a2373bc09e01ea4a8c24f037
jakubwilk <jw@jakubwilk.pl>
2023-04-02 22:16:00 +0200

    Install swagger config, nestjs config, cookie parser and setup cors for API

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as cookieParser from 'cookie-parser'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
+  const config = new DocumentBuilder()
+    .setTitle('Mage Guild Wars API')
+    .setDescription('API description for MGW server')
+    .setVersion('1.0')
+    .addTag('mgw')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document)
+  app.use(cookieParser())
   await app.listen(3001)
 }
 bootstrap()
