a4826ed78b5dfad6775365f34f6015e55ffebc32
jakubwilk <jw@jakubwilk.pl>
2023-04-04 16:29:07 +0200

    Modify auth context and create empty mutation for user registration

--- client/src/modules/auth/models/auth.model.ts
+++ client/src/modules/auth/models/auth.model.ts
@@ -1,14 +1,6 @@
-export enum AuthActionsEnum {
-  LOGIN = 'LOGIN',
-  LOGOUT = 'LOGOUT',
-  REGISTER = 'REGISTER',
-  FORGET_PASSWORD = 'FORGET_PASSWORD',
-  NO_ACCESS = 'NO_ACCESS',
-  ERROR_CODE = 'ERROR_CODE',
-}
-
 export interface AuthContextType {
   isUser: boolean
   user: object | null
-  dispatchAction: (action: AuthActionsEnum) => void
+  setIsUser: (v: boolean) => void
+  setUser: (v: object | null) => void
 }
