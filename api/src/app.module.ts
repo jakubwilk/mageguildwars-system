9e2cf97f537c12ba0aa2b574b714874502fa3dbc
jakubwilk <jw@jakubwilk.pl>
2023-04-04 22:50:34 +0200

    Update path in tsconfig for api project

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,6 @@
+import { AuthModule } from '@auth/auth.module'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-
-import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule],
