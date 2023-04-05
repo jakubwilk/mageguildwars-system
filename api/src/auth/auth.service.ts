a11a4a28f6de07f2dd622045322dc75908f9bba8
jakubwilk <jw@jakubwilk.pl>
2023-04-05 22:00:45 +0200

    Przejście na MySQL i poprawka do serwisu tworzenia użytkownika

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,26 +1,34 @@
 import { CreateAccountRequestParams } from '@auth/models'
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { Prisma, User } from '@prisma/client'
+import { PrismaService } from '@prisma/prisma.service'
 import * as argon2 from 'argon2'
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private prismaService: PrismaService) {}
 
   async createPasswordHash(password: string) {
     try {
       return await argon2.hash(password)
     } catch (err) {
-      console.log('err', err)
       throw new HttpException('Wystąpił błąd przy tworzeniu konta użytkownika. Kod błędu: LDE_USERx001a', HttpStatus.REQUEST_TIMEOUT)
     }
   }
 
   async createAccount({ login, email, password }: CreateAccountRequestParams) {
     try {
-      console.log('login', login)
-      console.log('email', email)
-      await this.createPasswordHash(password)
+      const hashedPassword = await this.createPasswordHash(password)
+      const dataToCreate: Prisma.UserCreateInput = {
+        login,
+        email,
+        password: hashedPassword,
+      }
+      const user: User = await this.prismaService.user.create({
+        data: { ...dataToCreate },
+      })
+      console.log('user', user)
       return { user: 'Vincent', role: 'OPERATOR' }
     } catch {
       throw new HttpException('Wystąpił błąd przy tworzeniu konta użytkownika. Kod błędu: LDE_USERx001', HttpStatus.BAD_REQUEST)
