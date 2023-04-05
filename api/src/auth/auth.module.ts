822ad385a19759205c07c304bd595c26008a3a2f
jakubwilk <jw@jakubwilk.pl>
2023-04-05 07:56:15 +0200

    Install @nestjs/jwt

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
 import { PrismaService } from '@prisma/prisma.service'
 
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env['JWT_SECRET'],
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   providers: [PrismaService, AuthService],
   controllers: [AuthController],
 })
