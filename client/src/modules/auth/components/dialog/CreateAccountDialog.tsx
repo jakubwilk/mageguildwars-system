96a12b628955c0d2b70e992baf52e9b718c5988c
jakubwilk <jw@jakubwilk.pl>
2023-04-05 16:04:24 +0200

    Install react-hook-form to handle forms, joi for validation and update registration form

--- client/src/modules/auth/components/dialog/CreateAccountDialog.tsx
+++ client/src/modules/auth/components/dialog/CreateAccountDialog.tsx
@@ -1,10 +1,55 @@
-import { Dialog } from '@common'
+import { useCallback } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { Dialog, TextField } from '@common'
+import { joiResolver } from '@hookform/resolvers/joi'
+import { Button } from '@mantine/core'
+import Joi from 'joi'
 
 function CreateAccountDialog() {
+  const form = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      login: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+    },
+    resolver: joiResolver(
+      Joi.object({
+        login: Joi.string().required().messages({ 'string.empty': 'Pole jest wymagane' }),
+        email: Joi.string()
+          .email({ tlds: false })
+          .messages({ 'string.empty': 'Pole jest wymagane', 'string.email': 'Adres email jest niepoprawny' }),
+        password: Joi.string()
+          .min(12)
+          .messages({ 'string.empty': 'Pole jest wymagane', 'string.min': 'Hasło powinno zawierać minimum 12 znaków' }),
+        repeatPassword: Joi.string()
+          .valid(Joi.ref('password'))
+          .required()
+          .messages({ 'string.empty': 'Pole jest wymagane', 'any.only': 'Hasła nie są identyczne' }),
+      })
+    ),
+  })
+
+  const handleCreateAccount = useCallback((data: any) => console.log('data', data), [])
+
   return (
-    <Dialog title={'Zakładanie konta'}>
-      <p>{'Zakładanie konta'}</p>
-    </Dialog>
+    <FormProvider {...form}>
+      <Dialog title={'Zakładanie konta'}>
+        <form onSubmit={form.handleSubmit(handleCreateAccount)}>
+          <TextField
+            label={'Nazwa użytkownika'}
+            name={'login'}
+            placeholder={'Podaj nazwę użytkownka, która posłuży również za imię postaci'}
+            isRequired
+          />
+          <TextField label={'Email'} name={'email'} placeholder={'Podaj swój adres email'} isRequired />
+          <TextField label={'Hasło'} name={'password'} type={'password'} isRequired isPasswordInput />
+          <TextField label={'Potwierdź hasło'} name={'repeatPassword'} type={'password'} isRequired isPasswordInput />
+          <Button type={'submit'}>{'Utwórz konto'}</Button>
+        </form>
+      </Dialog>
+    </FormProvider>
   )
 }
 
--- client/src/modules/common/components/form/inputs/TextField.tsx
+++ client/src/modules/common/components/form/inputs/TextField.tsx
@@ -1,20 +1,87 @@
-import { Input } from '@mantine/core'
+import { HTMLInputTypeAttribute, ReactNode } from 'react'
+import { Controller } from 'react-hook-form'
+import { Input, PasswordInput } from '@mantine/core'
 import { useId } from '@mantine/hooks'
+import { MantineNumberSize, MantineSize } from '@mantine/styles'
 
 interface IProps {
   label: string
   name: string
-  placeholder: string
+  placeholder?: string
+  type?: HTMLInputTypeAttribute
+  icon?: ReactNode
+  size?: MantineSize
+  radius?: MantineNumberSize
+  className?: string
   isRequired?: boolean
+  isPointer?: boolean
+  isMultiline?: boolean
+  isDisabled?: boolean
+  isPasswordInput?: boolean
 }
 
-function TextField({ label, name, placeholder, isRequired }: IProps) {
+function TextField({
+  label,
+  name,
+  placeholder,
+  type,
+  icon,
+  size = 'md',
+  radius,
+  className,
+  isRequired,
+  isPointer,
+  isMultiline,
+  isDisabled,
+  isPasswordInput,
+}: IProps) {
   const id = useId()
 
   return (
-    <Input.Wrapper id={id} label={label} required={isRequired}>
-      <Input id={id} name={name} placeholder={placeholder} />
-    </Input.Wrapper>
+    <Controller
+      name={name}
+      render={({ field: { name, value, onChange, onBlur }, fieldState: { error } }) => (
+        <Input.Wrapper id={id} label={label} required={isRequired}>
+          {isPasswordInput ? (
+            <PasswordInput
+              id={id}
+              value={value}
+              name={name}
+              onChange={onChange}
+              onBlur={onBlur}
+              placeholder={placeholder}
+              error={!!error}
+              icon={icon}
+              size={size}
+              radius={radius}
+              className={className}
+              autoComplete={'off'}
+              disabled={isDisabled}
+            />
+          ) : (
+            <Input
+              id={id}
+              value={value}
+              name={name}
+              type={type}
+              onChange={onChange}
+              onBlur={onBlur}
+              placeholder={placeholder}
+              error={!!error}
+              icon={icon}
+              size={size}
+              radius={radius}
+              className={className}
+              autoComplete={'off'}
+              pointer={isPointer}
+              multiline={isMultiline}
+              disabled={isDisabled}
+            />
+          )}
+          {error && <Input.Error>{error.message}</Input.Error>}
+        </Input.Wrapper>
+      )}
+    />
   )
 }
 
