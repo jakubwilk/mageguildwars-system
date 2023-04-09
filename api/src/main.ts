1c802b82d0f0f0d854b9876a0684153c522dd2ca
jakubwilk <jw@jakubwilk.pl>
2023-04-09 17:50:48 +0200

    Update README.md and main.ts in api project

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true })
+  app.enableCors({ origin: process.env['CLIENT_URL'], credentials: true })
   app.use(cookieParser())
   await app.listen(3001)
 }
