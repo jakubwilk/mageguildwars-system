cf23de1d0e4123b12e4940d7f3468425af68db23
jakubwilk <jw@jakubwilk.pl>
2023-04-09 22:50:06 +0200

    Update cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,21 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: process.env['CLIENT_URL'], credentials: true })
+  app.enableCors({
+    origin: process.env['CLIENT_URL'],
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Credential',
+      'Access-Control-Allow-Methods',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Request-Method',
+      'Access-Control-Request-Headers',
+    ],
+  })
   app.use(cookieParser())
   await app.listen(3001)
 }
