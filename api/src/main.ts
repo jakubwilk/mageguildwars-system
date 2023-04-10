c32f040570704d2df5f6ffba17cee093d40842ee
jakubwilk <jw@jakubwilk.pl>
2023-04-10 18:16:40 +0200

    Cors issue - maybe fix v3

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,10 +6,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: '*',
-    preflightContinue: true,
-    methods: '*',
-    allowedHeaders: '*',
+    origin: process.env['CLIENT_URL'],
   })
   app.use(cookieParser())
   await app.listen(parseInt(process.env['API_PORT']))
