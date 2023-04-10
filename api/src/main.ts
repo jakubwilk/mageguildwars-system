2f447a16be19cf8970cb4149a239d7ea826d16af
jakubwilk <jw@jakubwilk.pl>
2023-04-10 16:56:03 +0200

    Update header

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
     methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
   app.use(cookieParser())
   await app.listen(parseInt(process.env['API_PORT']))
 }
