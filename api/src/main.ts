ce107c4bd99f7ac44e29f39577cc556c4ddc3a18
jakubwilk <jw@jakubwilk.pl>
2023-04-10 17:26:27 +0200

    Cors issue - maybe fix v2

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,8 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
     origin: '*',
-    methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
+    preflightContinue: true,
+    methods: '*',
+    allowedHeaders: '*',
   })
   app.use(cookieParser())
   await app.listen(parseInt(process.env['API_PORT']))
