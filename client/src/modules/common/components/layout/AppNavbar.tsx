ad2cf3917a8cb49f51161de351f7763b0c32099e
jakubwilk <jw@jakubwilk.pl>
2023-04-14 22:43:55 +0200

    Update layout

--- client/src/modules/common/components/layout/AppNavbar.tsx
+++ client/src/modules/common/components/layout/AppNavbar.tsx
@@ -1,20 +1,22 @@
 import { useAuthContext } from '@auth'
-import { Navbar } from '@mantine/core'
+import { Drawer, Navbar } from '@mantine/core'
 import { GuestUserNavigation, LoggedUserNavigation } from '@user'
 
 interface IProps {
   isOpen: boolean
+  handleClose: () => void
 }
 
-function AppNavbar({ isOpen }: IProps) {
-  // eslint-disable-next-line no-unused-vars
-  const { isUser, setUser } = useAuthContext()
+function AppNavbar({ isOpen, handleClose }: IProps) {
+  const { isUser } = useAuthContext()
 
   return (
-    <Navbar hiddenBreakpoint={'sm'} hidden={!isOpen} width={{ sm: 300, lg: 350 }} className={'p-4'}>
-      <Navbar.Section grow>{'Główna nawigacja'}</Navbar.Section>
-      <Navbar.Section>{isUser ? <LoggedUserNavigation /> : <GuestUserNavigation />}</Navbar.Section>
-    </Navbar>
+    <Drawer opened={isOpen} onClose={handleClose} title={'Nawigacja'}>
+      <Navbar className={'border-0'}>
+        <Navbar.Section>{'Główna nawigacja'}</Navbar.Section>
+        <Navbar.Section>{isUser ? <LoggedUserNavigation /> : <GuestUserNavigation />}</Navbar.Section>
+      </Navbar>
+    </Drawer>
   )
 }
 
