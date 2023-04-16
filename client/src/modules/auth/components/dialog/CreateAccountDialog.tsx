c10de92f811e199f79bab75eb370953b953749ce
jakubwilk <jw@jakubwilk.pl>
2023-04-16 20:35:45 +0200

    Change form hook in registration form

--- client/src/modules/auth/components/dialog/CreateAccountDialog.tsx
+++ client/src/modules/auth/components/dialog/CreateAccountDialog.tsx
@@ -1,5 +1,4 @@
 import { ReactNode, useCallback, useEffect } from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import {
   authService,
@@ -9,12 +8,37 @@ import {
   useAuthContext,
   useCreateAccountMutation,
 } from '@auth'
-import { MIN_PASSWORD_LENGTH, TextField, typedFieldName } from '@common'
-import { joiResolver } from '@hookform/resolvers/joi'
-import { Button, Grid } from '@mantine/core'
+import { MIN_PASSWORD_LENGTH, typedFieldName } from '@common'
+import { Button, createStyles, Grid, TextInput } from '@mantine/core'
+import { useForm, yupResolver } from '@mantine/form'
 import { notifications } from '@mantine/notifications'
 import { clsx } from 'clsx'
-import Joi from 'joi'
+import * as Yup from 'yup'
+
+const useStyles = createStyles((theme) => ({
+  input: {
+    '& .mantine-TextInput-label, & .mantine-PasswordInput-label': {
+      color: theme.colors.gray[6],
+      marginBottom: 8,
+      fontSize: '0.85rem',
+    },
+    '& .mantine-TextInput-input, & .mantine-PasswordInput-input': {
+      backgroundColor: theme.colors.gray[3],
+      borderColor: theme.colors.gray[3],
+    },
+    '& .mantine-TextInput-error, & .mantine-PasswordInput-error': {
+      fontSize: '0.8rem',
+    },
+  },
+  button: {
+    fontSize: '0.85rem',
+    fontWeight: 500,
+    backgroundColor: theme.colors['dark-purple'][9],
+    '&:hover, &:focus': {
+      backgroundColor: theme.colors['dark-purple'][7],
+    },
+  },
+}))
 
 interface IProps {
   closeButton: ReactNode
@@ -23,32 +47,30 @@ interface IProps {
 
 function CreateAccountDialog({ closeButton, handleCloseDialog }: IProps) {
   const { t } = useTranslation()
+  const { classes } = useStyles()
   const { setUser } = useAuthContext()
   const { mutate: createAccount, isLoading } = useCreateAccountMutation()
 
   const form = useForm({
-    mode: 'onBlur',
-    defaultValues: CREATE_ACCOUNT_INITIAL_VALUES,
-    resolver: joiResolver(
-      Joi.object({
-        login: Joi.string()
-          .required()
-          .messages({ 'string.empty': t('validation:fieldIsRequired') }),
-        email: Joi.string()
-          .email({ tlds: false })
-          .messages({ 'string.empty': t('validation:fieldIsRequired'), 'string.email': t('validation:wrongEmailAddress') }),
-        password: Joi.string()
-          .min(MIN_PASSWORD_LENGTH)
-          .messages({
-            'string.empty': t('validation:fieldIsRequired'),
-            'string.min': t('validation:fieldShouldContainAtLeastCharacters', {
+    initialValues: CREATE_ACCOUNT_INITIAL_VALUES,
+    validate: yupResolver(
+      Yup.object().shape({
+        login: Yup.string().required(t('validation:fieldIsRequired') as string),
+        email: Yup.string()
+          .email(t('validation:wrongEmailAddress') as string)
+          .required(t('validation:fieldIsRequired') as string),
+        password: Yup.string()
+          .min(
+            MIN_PASSWORD_LENGTH,
+            t('validation:fieldShouldContainAtLeastCharacters', {
               field: t('auth:field.password'),
               characters: MIN_PASSWORD_LENGTH,
-            }),
-          }),
-        repeatPassword: Joi.string()
-          .valid(Joi.ref('password'))
-          .messages({ 'string.empty': t('validation:fieldIsRequired'), 'any.only': t('validation:passwordAreNotTheSame') }),
+            }) as string
+          )
+          .required(t('validation:fieldIsRequired') as string),
+        // repeatPassword: Yup.string()
+        //   .valid(Yup.ref('password'))
+        //   .messages({ 'string.empty': t('validation:fieldIsRequired'), 'any.only': t('validation:passwordAreNotTheSame') }),
       })
     ),
   })
@@ -78,42 +100,58 @@ function CreateAccountDialog({ closeButton, handleCloseDialog }: IProps) {
   }, [])
 
   return (
-    <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(handleCreateAccount)}>
-        <Grid grow>
-          <Grid.Col span={12}>
-            <TextField label={t('auth:field.login')} name={typedFieldName<CreateAccountForm>('login')} isRequired />
-          </Grid.Col>
-          <Grid.Col span={12}>
-            <TextField label={t('auth:field.email')} name={typedFieldName<CreateAccountForm>('email')} isRequired />
-          </Grid.Col>
-          <Grid.Col span={12}>
-            <TextField
-              label={t('auth:field.password')}
-              name={typedFieldName<CreateAccountForm>('password')}
-              type={'password'}
-              isRequired
-              isPasswordInput
-            />
-          </Grid.Col>
-          <Grid.Col span={12}>
-            <TextField
-              label={t('auth:field.repeatPassword')}
-              name={typedFieldName<CreateAccountForm>('repeatPassword')}
-              type={'password'}
-              isRequired
-              isPasswordInput
-            />
-          </Grid.Col>
-        </Grid>
-        <div className={'flex items-center justify-end mt-4'}>
-          {closeButton}
-          <Button type={'submit'} className={clsx('duration-100 ml-4')} loading={isLoading}>
-            {t('common:action.register')}
-          </Button>
-        </div>
-      </form>
-    </FormProvider>
+    <form onSubmit={form.onSubmit(handleCreateAccount)}>
+      <Grid grow>
+        <Grid.Col span={12}>
+          <TextInput
+            {...form.getInputProps('login')}
+            label={t('auth:field.login')}
+            name={typedFieldName<CreateAccountForm>('login')}
+            className={classes.input}
+            size={'md'}
+            withAsterisk
+          />
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <TextInput
+            {...form.getInputProps('email')}
+            label={t('auth:field.email')}
+            name={typedFieldName<CreateAccountForm>('email')}
+            className={classes.input}
+            size={'md'}
+            withAsterisk
+          />
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <TextInput
+            {...form.getInputProps('password')}
+            label={t('auth:field.password')}
+            name={typedFieldName<CreateAccountForm>('password')}
+            className={classes.input}
+            size={'md'}
+            withAsterisk
+          />
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <TextInput
+            {...form.getInputProps('repeatPassword')}
+            label={t('auth:field.repeatPassword')}
+            name={typedFieldName<CreateAccountForm>('repeatPassword')}
+            className={classes.input}
+            size={'md'}
+            withAsterisk
+          />
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <div className={'flex items-center justify-end mt-4'}>
+            {closeButton}
+            <Button type={'submit'} className={clsx('duration-100 ml-4')} loading={isLoading}>
+              {t('common:action.register')}
+            </Button>
+          </div>
+        </Grid.Col>
+      </Grid>
+    </form>
   )
 }
 
