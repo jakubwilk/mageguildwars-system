80cf8a733bc67b57c1cc314565ed431dfc9a0909
jakubwilk <jw@jakubwilk.pl>
2023-04-17 23:03:22 +0200

    Install date-fns and modify feature panels

--- client/src/modules/feature-panel/components/FeaturePanel.tsx
+++
@@ -1,5 +0,0 @@
-function FeaturePanel() {
-  return <div className={'container mx-auto my-4'}>{'Feature panel'}</div>
-}
-
-export default FeaturePanel
--- client/src/modules/feature-panel/hooks/useFeaturePanels.tsx
+++ client/src/modules/feature-panel/hooks/useFeaturePanels.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react'
+
+import { FEATURE_PANELS_MOCK } from '../mock'
+import { SingleFeaturePanel } from '../models'
+
 function useFeaturePanels() {
-  return {}
+  const [data, setData] = useState<Array<SingleFeaturePanel>>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetch = setTimeout(() => {
+      setData(FEATURE_PANELS_MOCK)
+      setIsLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(fetch)
+  }, [])
+
+  return { isLoading, data }
 }
 
 export default useFeaturePanels
