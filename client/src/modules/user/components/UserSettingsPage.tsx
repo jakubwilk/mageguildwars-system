8baba89b16484c64fd31641ac7a3df464eec24fb
jakubwilk <jw@jakubwilk.pl>
2023-04-26 21:31:29 +0200

    Change styling in poc user settings page

--- client/src/modules/user/components/UserSettingsPage.tsx
+++ client/src/modules/user/components/UserSettingsPage.tsx
@@ -3,10 +3,14 @@ import { Helmet } from 'react-helmet-async'
 import { useAuthContext } from '@auth'
 import { useAppLayoutContext } from '@common'
 import { createStyles } from '@mantine/core'
+import { clsx } from 'clsx'
 
 const useStyles = createStyles((theme) => ({
   page: {
     border: `1px solid ${theme.colors.gray[9]}`,
+  },
+  title: {
+    marginTop: 0,
   },
 }))
 
@@ -26,8 +30,8 @@ function UserPage() {
         <title>{`Panel użytkownika ${user?.login} | Mage Guild Wars`}</title>
       </Helmet>
       <div className={'container mx-auto my-8'}>
-        <div className={classes.page}>
-          <p>{'User Settings'}</p>
+        <div className={clsx('p-4', classes.page)}>
+          <h2 className={clsx('mb-4 uppercase', classes.title)}>{`Ustawienia konta ${user?.login}`}</h2>
         </div>
       </div>
     </Fragment>
