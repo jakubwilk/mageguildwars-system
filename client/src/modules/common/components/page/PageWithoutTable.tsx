407addf4b1773d1c09a5a25f0fc1b40d6dedc9df
jakubwilk <jw@jakubwilk.pl>
2023-06-01 09:48:43 +0200

    Fix styling in profile page

--- client/src/modules/common/components/page/PageWithoutTable.tsx
+++ client/src/modules/common/components/page/PageWithoutTable.tsx
@@ -6,9 +6,11 @@ const useStyles = createStyles((theme) => ({
   title: {
     fontSize: '2rem',
     color: theme.colors.gray[2],
+    wordBreak: 'break-all',
   },
   breadcrumbs: {
     color: theme.colors.gray[6],
+    wordBreak: 'break-all',
     '& a:link, a:visited': {
       color: theme.colors.gray[7],
       textDecoration: 'none',
@@ -37,12 +39,12 @@ function PageWithoutTable({ title, breadcrumbs, children, headerActions, isLoadi
         {isLoading ? (
           <Skeleton height={100} radius={'md'} />
         ) : (
-          <div className={'flex items-center justify-between'}>
+          <div className={'flex items-center flex-col md:justify-between md:flex-row'}>
             <div>
               <h2 className={clsx('mt-0 mb-4', classes.title)}>{title}</h2>
               <Breadcrumbs className={clsx(classes.breadcrumbs)}>{breadcrumbs}</Breadcrumbs>
             </div>
-            {headerActions && <div className={'ml-8'}>{headerActions}</div>}
+            {headerActions && <div className={'ml-8 mt-4 md:mt-0 w-full md:w-auto'}>{headerActions}</div>}
           </div>
         )}
       </header>
