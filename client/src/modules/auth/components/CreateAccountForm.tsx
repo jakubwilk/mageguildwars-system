8d2569710d7a8efd522244dd6062769bd5518c5b
jakubwilk <jw@jakubwilk.pl>
2023-06-16 21:48:30 +0200

    Create register form with integrated API

---
+++ client/src/modules/auth/components/CreateAccountForm.tsx
@@ -0,0 +1,62 @@
+import { useCallback, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+import {
+  CREATE_ACCOUNT_INITIAL_VALUES,
+  CreateAccountFormContent,
+  CreateAccountRequestParams,
+  createAccountSchema,
+  useAuthContext,
+  useCreateAccountMutation,
+} from '@auth'
+import { Logo } from '@common'
+import { Tooltip } from '@mantine/core'
+import { useForm, yupResolver } from '@mantine/form'
+import { notifications } from '@mantine/notifications'
+
+function CreateAccountForm() {
+  const { t } = useTranslation()
+  const { setUser } = useAuthContext()
+  const { mutate: createAccount, isLoading } = useCreateAccountMutation()
+  const form = useForm<CreateAccountRequestParams>({
+    initialValues: CREATE_ACCOUNT_INITIAL_VALUES,
+    validate: yupResolver(createAccountSchema),
+  })
+
+  const handleCreateAccountSubmit = useCallback(
+    (values: CreateAccountRequestParams) => {
+      createAccount(values, {
+        onSuccess: ({ data }) => {
+          setUser(data.user)
+          notifications.show({
+            message: t('auth:message.accountCreatedSuccessfully'),
+            color: 'green',
+            autoClose: 5000,
+            withCloseButton: true,
+          })
+        },
+      })
+    },
+    [createAccount, setUser, t]
+  )
+
+  useEffect(() => {
+    form.clearErrors()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div className={'flex flex-col items-center w-full'}>
+      <Tooltip label={t('common:mainPage.title')}>
+        <Link to={'/'} className={'mb-4'}>
+          <Logo />
+        </Link>
+      </Tooltip>
+      <form onSubmit={form.onSubmit(handleCreateAccountSubmit)}>
+        <CreateAccountFormContent form={form} isButtonLoading={isLoading} />
+      </form>
+    </div>
+  )
+}
+
+export default CreateAccountForm
--- client/src/modules/auth/pages/CreateAccountPage.tsx
+++ client/src/modules/auth/pages/CreateAccountPage.tsx
@@ -2,6 +2,8 @@ import { Fragment } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 
+import { AuthPageWrapper, CreateAccountForm } from '../components'
+
 function CreateAccountPage() {
   const { t } = useTranslation()
 
@@ -10,6 +12,9 @@ function CreateAccountPage() {
       <Helmet>
         <title>{t('auth:pages.register.title')}</title>
       </Helmet>
+      <AuthPageWrapper>
+        <CreateAccountForm />
+      </AuthPageWrapper>
     </Fragment>
   )
 }
