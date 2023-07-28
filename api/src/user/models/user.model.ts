004e897c8051d20f1a8bf3cf026c873b3f7052e2
jakubwilk <jw@jakubwilk.pl>
2023-07-28 20:13:38 +0200

    Add DTOs to all active routes in auth module

--- api/src/user/models/user.model.ts
+++ api/src/user/models/user.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { UserGroup } from '@prisma/client'
 
 export interface UserSnapshot {
@@ -12,5 +13,66 @@ export interface UserSnapshot {
   isGameMasterProfileEnabled: boolean
   isGameMaster: boolean
   createdAt: Date
+  updatedAt: Date
+}
+
+// R E F A C T O R
+export class UserSnapshotDto {
+  @ApiProperty({
+    description: 'Unique user ID',
+  })
+  uid: string
+
+  @ApiProperty({
+    description: 'Account username and first profile name',
+  })
+  login: string
+
+  @ApiProperty({
+    description: 'User slug used in URLs',
+  })
+  slug: string
+
+  @ApiProperty({
+    description: 'User email',
+  })
+  email: string
+
+  @ApiProperty({
+    description: 'Account group',
+  })
+  group: UserGroup
+
+  @ApiProperty({
+    description: 'Check if account is activated by admins',
+  })
+  isActive: boolean
+
+  @ApiProperty({
+    description: 'Check if account is banned or temporary locked',
+  })
+  isLocked: boolean
+
+  @ApiProperty({
+    description: 'Check if user is allowed to create more profiles',
+  })
+  isCreateProfileEnabled: boolean
+
+  @ApiProperty({
+    description: 'Check if user is allowed to create game master profile',
+  })
+  isGameMasterProfileEnabled: boolean
+
+  @ApiProperty({
+    description: 'Check if account is game master. TODO: To remove',
+  })
+  isGameMaster: boolean
+
+  @ApiProperty({ description: 'Date when account was created' })
+  createdAt: Date
+
+  @ApiProperty({
+    description: 'Date of last account update',
+  })
   updatedAt: Date
 }
