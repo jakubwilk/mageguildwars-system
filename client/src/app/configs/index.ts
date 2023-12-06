fb1bf0bc86fd3cccf5a51f640a9fa13fd1ac7d0b
jakubwilk <jw@jakubwilk.pl>
2023-12-06 10:36:48 +0100

    Merge from CRA to Nextjs and reset all

--- client/src/app/configs/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as axiosApi } from './axios.config'
-export { default as i18n } from './i18n.config'
-export * from './routes.config'
-export * from './theme.config'
--- client/src/app/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as HomePage } from './home/HomePage'
-export { default as RootPage } from './root/RootPage'
