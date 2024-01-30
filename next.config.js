ced7c8847c84ee5e02df3d4ba5898560fbb524bd
jakubwilk <jw@jakubwilk.pl>
2024-01-30 20:51:30 +0100

    Add imgur to next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
