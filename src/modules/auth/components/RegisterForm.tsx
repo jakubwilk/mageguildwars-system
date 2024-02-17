19aa2e1867e54754b73d2b6a543bff2855135fd7
jakubwilk <jw@jakubwilk.pl>
2024-02-17 14:42:31 +0100

    Add resources for register form fields

--- src/modules/auth/components/RegisterForm.tsx
+++ src/modules/auth/components/RegisterForm.tsx
@@ -2,11 +2,14 @@ import { useCallback, useMemo } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Group } from '@mantine/core'
+import { isEqual } from 'lodash'
 import { object, string } from 'yup'
 
 import { Button, PasswordInputField, TextInputField } from '../../common/components'
+import { useResources } from '../../resources/hooks'
 
 export function RegisterForm() {
+  const { getResource } = useResources('AUTH')
   const form = useForm({
     mode: 'onChange',
     criteriaMode: 'all',
@@ -17,9 +20,25 @@ export function RegisterForm() {
     },
     resolver: yupResolver(
       object({
-        email: string().email().required().nullable(),
-        password: string().required().nullable(),
-        repeatPassword: string().required().nullable(),
+        email: string()
+          .required(getResource('REGISTER_FORM_FIELD_REQUIRED_VALIDATION_TEXT'))
+          .email(getResource('REGISTER_FORM_FIELD_EMAIL_VALIDATION_TEXT'))
+          .nullable(),
+        password: string()
+          .required(getResource('REGISTER_FORM_FIELD_REQUIRED_VALIDATION_TEXT'))
+          .min(10, getResource('REGISTER_FORM_FIELD_PASSWORD_VALIDATION_TEXT'))
+          .nullable(),
+        repeatPassword: string()
+          .required(getResource('REGISTER_FORM_FIELD_REQUIRED_VALIDATION_TEXT'))
+          .test(
+            'checkIfPasswordsAreTheSame',
+            getResource('REGISTER_FORM_FIELD_REPEAT_PASSWORD_VALIDATION_TEXT'),
+            function (value, context) {
+              const passwordValue = context.parent.password
+              return isEqual(value, passwordValue)
+            },
+          )
+          .nullable(),
       }),
     ),
   })
@@ -35,27 +54,26 @@ export function RegisterForm() {
       <form onSubmit={form.handleSubmit(handleSubmit)}>
         <Group>
           <TextInputField
-            description={
-              'Służy nie tylko do odzyskania dostępu do konta, ale również głównie za pomocą adresu email użytkownik może się zalogować'
-            }
-            label={'Adres email'}
+            description={getResource('REGISTER_FORM_FIELD_EMAIL_DESCRIPTION_TEXT')}
+            label={getResource('REGISTER_FORM_FIELD_EMAIL_LABEL')}
             name={'email'}
             withAsterisk
           />
           <PasswordInputField
             className={'w-full'}
-            label={'Hasło'}
+            description={getResource('REGISTER_FORM_FIELD_PASSWORD_DESCRIPTION_TEXT')}
+            label={getResource('REGISTER_FORM_FIELD_PASSWORD_LABEL')}
             name={'password'}
             withAsterisk
           />
           <PasswordInputField
             className={'w-full'}
-            label={'Powtórz hasło'}
+            label={getResource('REGISTER_FORM_FIELD_REPEAT_PASSWORD_LABEL')}
             name={'repeatPassword'}
             withAsterisk
           />
           <Button className={'mt-2'} isFullWidth type={'submit'}>
-            {'Utwórz konto'}
+            {getResource('REGISTER_FORM_BUTTON_CREATE_ACCOUNT_TEXT')}
           </Button>
         </Group>
       </form>
--- src/modules/resources/constants/auth.resources.ts
+++ src/modules/resources/constants/auth.resources.ts
@@ -2,4 +2,17 @@ export enum AuthResourcesEnum {
   REGISTER_WRAPPER_LOGO_ALT_TEXT = 'Srebrny napis Mage Guild Wars',
   REGISTER_WRAPPER_BACK_LINK_TEXT = 'Powrót na stronę główną',
   REGISTER_WRAPPER_BACK_LINK_TOOLTIP_TEXT = 'Wróć na stronę główną Mage Guild Wars',
+
+  REGISTER_FORM_FIELD_EMAIL_LABEL = 'Adres email',
+  REGISTER_FORM_FIELD_EMAIL_DESCRIPTION_TEXT = 'Służy nie tylko do odzyskania dostępu do konta, ale również głównie za pomocą adresu email użytkownik może się zalogować',
+  REGISTER_FORM_FIELD_PASSWORD_LABEL = 'Hasło',
+  REGISTER_FORM_FIELD_PASSWORD_DESCRIPTION_TEXT = 'Hasło powinno mieć długość co najmniej 10 znaków',
+  REGISTER_FORM_FIELD_REPEAT_PASSWORD_LABEL = 'Powtórz hasło',
+
+  REGISTER_FORM_FIELD_EMAIL_VALIDATION_TEXT = 'Niepoprawny adres email',
+  REGISTER_FORM_FIELD_PASSWORD_VALIDATION_TEXT = 'Niepoprawne hasło',
+  REGISTER_FORM_FIELD_REPEAT_PASSWORD_VALIDATION_TEXT = 'Hasło nie jest identyczne',
+  REGISTER_FORM_FIELD_REQUIRED_VALIDATION_TEXT = 'Pole jest wymagane',
+
+  REGISTER_FORM_BUTTON_CREATE_ACCOUNT_TEXT = 'Utwórz konto',
 }
