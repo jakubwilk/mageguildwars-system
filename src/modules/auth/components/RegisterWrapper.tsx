974b2745980482582f5a1477f85d08ddf4025d39
jakubwilk <jw@jakubwilk.pl>
2024-02-17 14:01:05 +0100

    Apply resource to RegisterWrapper component

--- src/modules/auth/components/RegisterWrapper.tsx
+++ src/modules/auth/components/RegisterWrapper.tsx
@@ -3,11 +3,15 @@ import { Text, Tooltip } from '@mantine/core'
 import { IconArrowNarrowLeft } from '@tabler/icons-react'
 import clsx from 'clsx'
 
+import { useResources } from '../../resources/hooks'
+
 import { RegisterForm } from './RegisterForm.tsx'
 
 import classes from './Register.module.css'
 
 export function RegisterWrapper() {
+  const { getResource } = useResources('AUTH')
+
   return (
     <div
       className={clsx(
@@ -18,13 +22,16 @@ export function RegisterWrapper() {
       <div className={'m-8'}>
         <div className={'flex justify-center mb-8'}>
           <img
-            alt={'Srebrny napis Mage Guild Wars'}
+            alt={getResource('REGISTER_WRAPPER_LOGO_ALT_TEXT')}
             src={'https://mageguildwars.pl/images/mgw_modern/logo.png'}
           />
         </div>
         <RegisterForm />
         <div className={'flex justify-center'}>
-          <Tooltip color={'gray'} label={'Wróć na stronę główną Mage Guild Wars'}>
+          <Tooltip
+            color={'gray'}
+            label={getResource('REGISTER_WRAPPER_BACK_LINK_TOOLTIP_TEXT')}
+          >
             <Link
               className={clsx(
                 'flex items-center justify-center mt-8',
@@ -33,7 +40,9 @@ export function RegisterWrapper() {
               to={'/'}
             >
               <IconArrowNarrowLeft className={'mr-1'} stroke={1.5} />
-              <Text className={classes.backToHomeText}>{'Powrót na stronę główną'}</Text>
+              <Text className={classes.backToHomeText}>
+                {getResource('REGISTER_WRAPPER_BACK_LINK_TEXT')}
+              </Text>
             </Link>
           </Tooltip>
         </div>
