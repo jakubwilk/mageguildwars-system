38c72a8c06681a6faab3b5cc9e5876ab079ce14e
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-03-02 11:44:17 +0100

    Fix issue with console error about ref using Tooltip from Mantine

--- src/modules/user/components/menu/UserMenu.tsx
+++ src/modules/user/components/menu/UserMenu.tsx
@@ -8,48 +8,43 @@ import classes from './Menu.module.css'
 export function UserMenu() {
   return (
     <Group gap={'sm'}>
-      <Tooltip color={'gray'} label={'Otwórz menu użytkownika'}>
-        <Menu width={250}>
-          <Menu.Target>
-            <MantineButton
-              className={'p-0 m-0 h-[40px] w-[40px]'}
-              variant={'transparent'}
-            >
-              <Avatar radius={'xl'} />
-            </MantineButton>
-          </Menu.Target>
+      <Menu width={250}>
+        <Menu.Target>
+          <MantineButton className={'p-0 m-0 h-[40px] w-[40px]'} variant={'transparent'}>
+            <Avatar radius={'xl'} />
+          </MantineButton>
+        </Menu.Target>
 
-          <Menu.Dropdown>
-            <Menu.Label className={'uppercase'}>{'Konto'}</Menu.Label>
-            <Menu.Item component={Link} to={'/settings'}>
-              {'Edytuj dane'}
-            </Menu.Item>
-            <Menu.Item component={Link} to={'/'}>
-              {'Anonimizuj swoje dane'}
-            </Menu.Item>
-            <Menu.Label className={'uppercase'}>{'Mage Guild Wars'}</Menu.Label>
-            <Menu.Item component={Link} to={'/'}>
-              {'Character 1'}
-            </Menu.Item>
-            <Menu.Item component={Link} to={'/'}>
-              {'Character 2'}
-            </Menu.Item>
-            <Menu.Item component={Link} to={'/'}>
-              {'Character 3'}
-            </Menu.Item>
-            <Menu.Item component={Link} disabled={true} to={'/'}>
-              {'Dodaj nowe konto'}
-            </Menu.Item>
-            <Menu.Divider className={'h-[1px]'} />
-            <Menu.Item component={Link} to={'/'}>
-              {'Dziennik fabularny'}
-            </Menu.Item>
-            <Menu.Item component={Link} to={'/'}>
-              {'Panel Mistrza Gry'}
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </Tooltip>
+        <Menu.Dropdown>
+          <Menu.Label className={'uppercase'}>{'Konto'}</Menu.Label>
+          <Menu.Item component={Link} to={'/settings'}>
+            {'Edytuj dane'}
+          </Menu.Item>
+          <Menu.Item component={Link} to={'/'}>
+            {'Anonimizuj swoje dane'}
+          </Menu.Item>
+          <Menu.Label className={'uppercase'}>{'Mage Guild Wars'}</Menu.Label>
+          <Menu.Item component={Link} to={'/'}>
+            {'Character 1'}
+          </Menu.Item>
+          <Menu.Item component={Link} to={'/'}>
+            {'Character 2'}
+          </Menu.Item>
+          <Menu.Item component={Link} to={'/'}>
+            {'Character 3'}
+          </Menu.Item>
+          <Menu.Item component={Link} disabled={true} to={'/'}>
+            {'Dodaj nowe konto'}
+          </Menu.Item>
+          <Menu.Divider className={'h-[1px]'} />
+          <Menu.Item component={Link} to={'/'}>
+            {'Dziennik fabularny'}
+          </Menu.Item>
+          <Menu.Item component={Link} to={'/'}>
+            {'Panel Mistrza Gry'}
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
       <Tooltip color={'gray'} label={'Wyloguj się z aplikacji'}>
         <MantineButton className={classes.logout} variant={'transparent'}>
           <IconDoorExit stroke={1.5} style={{ height: '24px', width: '24px' }} />
