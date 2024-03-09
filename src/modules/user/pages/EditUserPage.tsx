27c1376317f543d6c43aaf6ef2261daae88e58e0
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-03-09 15:36:26 +0100

    Change grid columns

--- src/modules/user/pages/EditUserPage.tsx
+++ src/modules/user/pages/EditUserPage.tsx
@@ -1,12 +1,17 @@
-import { EditEmailBlock, EditPasswordBlock, EditUserHeader } from '../components'
+import {
+  EditEmailBlock,
+  EditPasswordBlock,
+  EditUserHeader,
+  EditUserInformationBlock,
+} from '../components'
 
 export function EditUserPage() {
   return (
-    <main className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'}>
+    <main className={'grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8'}>
       <EditUserHeader />
       <EditEmailBlock />
       <EditPasswordBlock />
-      <div>{'Informacje'}</div>
+      <EditUserInformationBlock />
     </main>
   )
 }
