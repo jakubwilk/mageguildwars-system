81acdd3b05c4fc68ebdbe5e337731a8ddd8ca7c9
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-03-16 14:47:06 +0100

    Create api global config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 
-import { APP_ROUTER } from './config/router.tsx'
+import { APP_ROUTER } from './config'
 
 export function App() {
   return <RouterProvider router={APP_ROUTER} />
