343a642ce98df7b90064ef21b9a409644c9386c5
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-04-10 14:53:29 +0200

    Rename utils

--- src/modules/common/utils/shared.uitls.ts
+++ src/modules/common/utils/shared.uitls.ts
@@ -1,3 +1,11 @@
 export const loadEnvVariable = (name: string): string => {
   return import.meta.env[name] || ''
 }
+
+export enum routeEnum {
+  HOME = '/',
+}
+
+export const ROUTES = {
+  home: () => '/',
+}
