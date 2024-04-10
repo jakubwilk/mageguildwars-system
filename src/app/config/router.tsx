9223df5632e205f1901eafbda0dd537f50b9ef8f
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-04-10 14:53:35 +0200

    Update routing

--- src/app/config/router.tsx
+++ src/app/config/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { RootLayout } from '../../modules/common/layout'
+import { routeEnum } from '../../modules/common/utils'
 import { HomePage } from '../pages'
 
 export const APP_ROUTER = createBrowserRouter([
   {
     element: <RootLayout />,
-    children: [{ element: <HomePage />, path: '/' }],
+    children: [{ element: <HomePage />, path: routeEnum.HOME }],
   },
 ])
