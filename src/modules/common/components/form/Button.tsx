c316a324f7a2705a9b674a51564c12b229de7687
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-04-12 10:09:07 +0200

    Fix issue with missing props for button component

--- src/modules/common/components/form/Button.tsx
+++ src/modules/common/components/form/Button.tsx
@@ -1,14 +1,13 @@
-import { MouseEvent, ReactNode } from 'react'
-import { Button as ButtonMantine, ButtonProps, UnstyledButtonProps } from '@mantine/core'
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { Button as ButtonMantine, ButtonProps } from '@mantine/core'
 
 import '@mantine/core/styles/Button.css'
 
-type TButtonProps = ButtonProps & UnstyledButtonProps
+type TButtonProps = ButtonProps & ComponentPropsWithoutRef<'button'>
 
 interface IProps extends TButtonProps {
   children: string | ReactNode
   type: 'button' | 'submit' | 'reset' | undefined
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
   isFullWidth?: boolean
 }
 
