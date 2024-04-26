ea15bdee562e72da97db3a63a0d7303c048e4de2
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-04-27 00:52:48 +0200

    Create utils functions for group and boolean values in edit user data modal

--- src/modules/user/components/UserInformation.tsx
+++ src/modules/user/components/UserInformation.tsx
@@ -1,20 +1,29 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { List } from '@mantine/core'
 import { IconClock, IconUser } from '@tabler/icons-react'
 import { Button } from 'common/components'
 import { closeUserSettingsModal } from 'common/store'
 import { useDispatch } from 'config'
 import { useResource } from 'resource/hooks'
+import { userBooleanColor, userGroupColor } from 'user/utils'
 
 import { UserInformationItem } from './UserInformationItem'
 
 export function UserInformation() {
   const { getResource } = useResource('COMMON')
+  const { getResource: getUserResource } = useResource('USER')
   const dispatch = useDispatch()
 
   const handleCloseUserSettingsModal = useCallback(
     () => dispatch(closeUserSettingsModal()),
     [dispatch],
+  )
+
+  const groupColor = useMemo(() => userGroupColor.get(2), [])
+
+  const getBooleanColor = useCallback(
+    (value: boolean) => userBooleanColor.get(value) || '',
+    [],
   )
 
   return (
@@ -24,38 +33,38 @@ export function UserInformation() {
           <UserInformationItem
             IconComponent={IconClock}
             badgeColor={'gray'}
-            label={'Data rejestracji:'}
+            label={getUserResource('MODAL_SETTINGS_INFO_REGISTER_DATE_TEXT')}
             value={new Date()}
           />
           <UserInformationItem
             IconComponent={IconClock}
             badgeColor={'gray'}
-            label={'Data ostatniej aktualizacji:'}
+            label={getUserResource('MODAL_SETTINGS_INFO_UPDATE_DATE_TEXT')}
             value={new Date()}
           />
           <UserInformationItem
             IconComponent={IconUser}
-            badgeColor={'blue'}
-            label={'Czy konto użytkownika posiada blokadę aktywacyjną:'}
+            badgeColor={getBooleanColor(true)}
+            label={getUserResource('MODAL_SETTINGS_INFO_ACTIVE_BLOCK_TEXT')}
             value={true}
           />
           <UserInformationItem
             IconComponent={IconUser}
-            badgeColor={'red'}
-            label={'Grupa:'}
+            badgeColor={groupColor || ''}
+            label={getUserResource('MODAL_SETTINGS_INFO_USER_GROUP_TEXT')}
             value={'Operator'}
           />
           <UserInformationItem
             IconComponent={IconUser}
-            badgeColor={'blue'}
-            label={'Czy użytkownik ma dostęp do panelu MG:'}
+            badgeColor={getBooleanColor(true)}
+            label={getUserResource('MODAL_SETTINGS_INFO_GAME_MASTER_PANEL_TEXT')}
             value={true}
           />
           <UserInformationItem
             IconComponent={IconUser}
-            badgeColor={'blue'}
-            label={'Czy użytkownik może tworzyć nowe postacie:'}
-            value={true}
+            badgeColor={getBooleanColor(false)}
+            label={getUserResource('MODAL_SETTINGS_INFO_CREATE_NEW_CHARACTERS_TEXT')}
+            value={false}
           />
         </List>
       </div>
---
+++ src/modules/user/utils/user.utils.ts
@@ -0,0 +1,11 @@
+export const userGroupColor = new Map([
+  [0, 'dark'],
+  [1, 'gray'],
+  [2, 'orange'],
+  [3, 'red'],
+])
+
+export const userBooleanColor = new Map([
+  [true, 'green'],
+  [false, 'blue'],
+])
