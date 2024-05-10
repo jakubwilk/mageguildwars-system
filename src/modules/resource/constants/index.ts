eaf37f971bd358bd41f6948e2c6f57b9808317d8
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-05-10 23:45:53 +0200

    Delete resources

--- src/modules/resource/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './common.resource.ts'
-export * from './auth.resource.ts'
-export * from './user.resource.ts'
-export * from './notification.resource.ts'
--- src/modules/resource/constants/notification.resource.ts
+++
@@ -1,5 +0,0 @@
-export enum NotificationResourceEnum {
-  NOTIFICATION_SUCCESS_TITLE_TEXT = 'Udało się',
-  NOTIFICATION_USER_LOGIN_SUCCESS_DESCRIPTION_TEXT = 'Użytkownik został pomyślnie zalogowany do aplikacji',
-  NOTIFICATION_USER_LOGOUT_SUCCESS_DESCRIPTION_TEXT = 'Użytkownik został pomyślnie wylogowany z aplikacji',
-}
