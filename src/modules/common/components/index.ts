311a014476e2ea8b063e08b6c91e6d132bed25fd
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-05-13 12:16:23 +0200

    Add NumberInputField and fix exports in components in common

--- src/modules/common/components/index.ts
+++ src/modules/common/components/index.ts
@@ -1,18 +1,7 @@
-export * from './footer/Footer.tsx'
-export * from './header/Header.tsx'
-
-export * from './socials/Discord.tsx'
-
-export * from './form/Button.tsx'
-export * from './form/TextInputField.tsx'
-export * from './form/PasswordInputField.tsx'
-export * from './form/CheckboxInputField.tsx'
-export * from './form/SelectInputField.tsx'
-export * from './form/DateInputField.tsx'
-
-export * from './menu/SidebarMenu.tsx'
-export * from './menu/MainMenu.tsx'
-
-export * from './content/Section.tsx'
-
-export * from './modal/Modal.tsx'
+export * from './footer'
+export * from './header'
+export * from './socials'
+export * from './form'
+export * from './menu'
+export * from './content'
+export * from './modal'
