4585a3ddbe6192f9417c4df3f1350962aae1b837
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-06-30 14:50:56 +0200

    Update login form

--- modules/auth/components/form/LoginForm.tsx
+++ modules/auth/components/form/LoginForm.tsx
@@ -1,19 +1,32 @@
 'use client'
 
+import Link from 'next/link'
 import { Text, Title } from '@mantine/core'
 import clsx from 'clsx'
+import { Button, CheckboxField, PasswordInputField, TextInputField } from 'common/components'
 
 import classes from './form.module.css'
 
 export default function LoginForm() {
   return (
-    <section className={'w-full md:flex md:justify-end bg-white p-8'}>
-      <header className={'w-full md:max-w-[90%] lg:max-w-[80%]'}>
-        <Text className={clsx('uppercase font-semibold tracking-widest text-xl mb-4', classes.subtitle)}>
-          {'Konto'}
-        </Text>
-        <Title className={'uppercase text-gray-800 text-4xl'}>{'Logowanie'}</Title>
-      </header>
+    <section className={'w-full md:flex md:flex-col md:items-end bg-white p-8'}>
+      <div className={'w-full md:max-w-[90%] lg:max-w-[80%]'}>
+        <header className={'mb-8'}>
+          <Text className={clsx('uppercase font-semibold tracking-widest text-xl mb-4', classes.subtitle)}>
+            {'Konto'}
+          </Text>
+          <Title className={'uppercase text-gray-800 text-4xl'}>{'Logowanie'}</Title>
+        </header>
+        <form className={'flex flex-col gap-4'}>
+          <TextInputField description={'Wprowadź adres email użyty podczas rejestracji'} label={'Adres email'} />
+          <PasswordInputField label={'Hasło użytkownika'} />
+          <CheckboxField label={'Zapamiętaj przy następnych logowaniach'} />
+          <div className={'flex items-center justify-between mt-4 gap-4'}>
+            <Button className={'w-full md:w-[auto]'}>{'Zaloguj się'}</Button>
+            <Link href={'/reset-password'}>{'Reset hasła'}</Link>
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
--- modules/auth/pages/LoginPage.tsx
+++ modules/auth/pages/LoginPage.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
   return (
     <main className={'w-full flex flex-col items-center md:grid md:grid-cols-2'}>
       <LoginForm />
-      <section className={'w-full md:flex md:justify-start bg-zinc-800 p-8'}>
+      <section className={'w-full h-full md:flex md:justify-start bg-zinc-800 p-8'}>
         <header className={'w-full md:max-w-[90%] lg:max-w-[80%]'}>
           <Text className={'uppercase font-semibold tracking-widest text-xl text-white text-opacity-50 mb-4'}>
             {'Konto'}
