f890a8f46733b07cf55e27fd727d0bfca9c8cb40
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-07-01 20:53:45 +0200

    Change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react'
-import { IBM_Plex_Sans } from 'next/font/google'
+import { Barlow } from 'next/font/google'
 import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import theme from 'config/theme'
 
 import '@mantine/core/styles.layer.css'
 import './globals.css'
 
-const ibm_plex_sand = IBM_Plex_Sans({
+const barlow = Barlow({
   subsets: ['latin-ext'],
   weight: ['300', '400', '600'],
   style: ['normal', 'italic']
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: Readonly<IProps>) {
       <head>
         <ColorSchemeScript />
       </head>
-      <body className={ibm_plex_sand.className}>
+      <body className={barlow.className}>
         <MantineProvider defaultColorScheme={'dark'} forceColorScheme={'dark'} theme={theme}>
           {children}
         </MantineProvider>
