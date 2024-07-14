43b6517cc377b750ad1cf371477927c08c4ddf30
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-07-14 10:30:54 +0200

    Add drawer

--- modules/common/components/layout/Navbar.tsx
+++ modules/common/components/layout/Navbar.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Drawer } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import { IconMenu2 } from '@tabler/icons-react'
 import clsx from 'clsx'
 import { Button, Menu } from 'common/components'
@@ -8,19 +10,26 @@ import { UserMenu } from 'user/components'
 import classes from './layout.module.css'
 
 export default function Navbar() {
+  const [opened, { open: handleOpenMenu, close: handleCloseMenu }] = useDisclosure(false)
+
   return (
-    <section className={'sticky top-0 px-4 md:px-8'}>
-      <div className={'container mx-auto'}>
-        <nav className={'relative bg-white h-[60px] flex justify-between gap-2 md:gap-4'}>
-          <div className={'flex items-center'}>
-            <Button className={clsx('h-[60px] w-[60px]', classes.navbarButton)}>
-              <IconMenu2 />
-            </Button>
-            <Menu isLinear />
-          </div>
-          <UserMenu />
-        </nav>
-      </div>
-    </section>
+    <>
+      <Drawer onClose={handleCloseMenu} opened={opened}>
+        <Menu isLinear={false} />
+      </Drawer>
+      <section className={'sticky top-0 px-4 md:px-8'}>
+        <div className={'container mx-auto'}>
+          <nav className={'relative bg-white h-[60px] flex justify-between gap-2 md:gap-4'}>
+            <div className={'flex items-center'}>
+              <Button className={clsx('h-[60px] w-[60px]', classes.navbarButton)} handleClick={handleOpenMenu}>
+                <IconMenu2 />
+              </Button>
+              <Menu isLinear />
+            </div>
+            <UserMenu />
+          </nav>
+        </div>
+      </section>
+    </>
   )
 }
