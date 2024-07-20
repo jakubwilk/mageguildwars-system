ee079474978cc80f2fa31d7884131c8d204059f8
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-07-20 23:03:47 +0200

    Add homepage title

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+import type { Metadata } from 'next'
 import { Header, Navbar } from 'common/components'
+
+export const metadata: Metadata = {
+  title: 'Mage Guild Wars | Strona główna',
+  description:
+    'Mage Guild Wars to gra PBF-lite inspirowana światem anime i mangi po tytułem Fairy Tail, w której Ty jako Gracz tworzysz własną postać w świecie pełnym magii i magicznych zjawisk'
+}
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <Navbar />
       <main className={'flex min-h-screen flex-col items-center justify-between p-24'}>
         <p>{'test'}</p>
       </main>
-    </div>
+    </>
   )
 }
