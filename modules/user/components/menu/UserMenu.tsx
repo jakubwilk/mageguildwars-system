67fde15d36809931f8abb7252e0823b528d45c2d
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-07-20 23:03:58 +0200

    Apply user menus

--- modules/user/components/menu/UserMenu.tsx
+++ modules/user/components/menu/UserMenu.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { useCallback } from 'react'
 import { Barlow } from 'next/font/google'
-import { Text } from '@mantine/core'
+import { Drawer, Text } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { IconUser } from '@tabler/icons-react'
 import clsx from 'clsx'
 import { Button } from 'common/components'
-import { useOutsideDivClick } from 'common/hooks'
+import { RegisterMenu } from 'user/components'
 
 import GuestMenu from './GuestMenu'
 
@@ -20,28 +19,27 @@ const barlow = Barlow({
 })
 
 export default function UserMenu() {
-  const [opened, handle] = useDisclosure()
-
-  const handleOpenMenuClick = useCallback(() => handle.open(), [handle])
-
-  const handleCloseMenuClick = useCallback(() => handle.close(), [handle])
-
-  const { ref } = useOutsideDivClick(handleCloseMenuClick)
+  const isUser = true
+  const [opened, { open: handleOpenMenu, close: handleCloseMenu }] = useDisclosure(false)
 
   return (
     <>
+      <Drawer
+        classNames={{ title: clsx('uppercase text-sm', classes.menuDrawerTitle) }}
+        onClose={handleCloseMenu}
+        opened={opened}
+        position={'right'}
+        title={'Menu użytkownika'}
+      >
+        {isUser ? <RegisterMenu /> : <GuestMenu />}
+      </Drawer>
       <Button
         className={clsx('flex items-center', classes.menuAccountButton, opened ? classes.menuAccountButtonActive : '')}
-        handleClick={handleOpenMenuClick}
+        handleClick={handleOpenMenu}
       >
         <IconUser />
         <Text className={clsx('ml-2', classes.menuButtonText, barlow.className)}>{'Konto'}</Text>
       </Button>
-      {opened && (
-        <div className={'bg-white z-10 absolute top-[60px] right-0 mt-4 min-w-[300px] flex flex-col'} ref={ref}>
-          <GuestMenu />
-        </div>
-      )}
     </>
   )
 }
